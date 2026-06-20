28e2a36e6e441984be90f8179890fef699e71464
Fasil Malik Hayat <fasil.hayat@gmail.com>
2026-06-20 20:26:52 +0200

    Add new tile assets and update TestMapData for improved world design

--- src/BattleArena.Gui/Models/World/TestMapData.cs
+++ src/BattleArena.Gui/Models/World/TestMapData.cs
@@ -11,14 +11,19 @@ public static class TestMapData
         for (var y = 0; y < height; y++)
         for (var x = 0; x < width; x++)
         {
-            // Dirt path down the center column
-            if (x == 7 || x == 8)
+            // Pond in the top-left corner
+            if (x >= 1 && x <= 3 && y >= 0 && y <= 2)
+                tiles[y * width + x] = new Tile(TileType.Water, 0, false);
+            // Mountain ridge across the top-right
+            else if (x >= 11 && x <= 14 && y <= 1)
+                tiles[y * width + x] = new Tile(TileType.Mountain, 0, false);
+            // Dirt path down the centre columns
+            else if (x == 7 || x == 8)
                 tiles[y * width + x] = new Tile(TileType.Road, 1, true);
-            // Shade variation: every other tile a slightly different green
-            else if ((x + y) % 3 == 0)
-                tiles[y * width + x] = new Tile(TileType.Grass, 1, true);
-            else if ((x + y) % 3 == 1)
-                tiles[y * width + x] = new Tile(TileType.Forest, 1, true);
+            // Scattered forest patches (impassable)
+            else if ((x + y) % 4 == 0 && x > 3 && x < 11)
+                tiles[y * width + x] = new Tile(TileType.Forest, 0, false);
+            // Grass everywhere else
             else
                 tiles[y * width + x] = new Tile(TileType.Grass, 1, true);
         }
